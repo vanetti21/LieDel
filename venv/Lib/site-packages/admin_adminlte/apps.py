c9280c85480dada871a1f94c4ae1f381372da7ec
vanetti21 <148173663+vanetti21@users.noreply.github.com>
2025-03-19 00:29:55 -0400

    Delete venv directory

--- venv/Lib/site-packages/admin_adminlte/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminAdminlteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_adminlte'
-    icon = 'fa fa-user'
